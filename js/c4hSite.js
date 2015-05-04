9dfeeb0c4c0086f0da17a63133ea6946bce9c55f
larrymason <larry.mason@code4huntsville.org>
2015-05-04 11:18:20 -0500

    changed top margin on text input filed to 5px
    also set timeout function on popup messages

--- js/c4hSite.js
+++ js/c4hSite.js
@@ -6,9 +6,17 @@ C4H.site.displayMessage = function(message) {
 	$("#glass").css("visibility", "visible");
     $("#messageArea").html(message);
     $("#popupMessage").css("display", "block");
-	          
+	setTimeout(function(){ C4H.site.displayMessage(); }, 15000);          
   } else {
 	$("#popupMessage").css("display", "none");
 	$("#glass").css("visibility", "hidden");
+	C4H.site.clearForms();
   }
+}
+
+C4H.site.clearForms = function() {
+  var fields = $("#s_name").add($("#s_email")).add($("#s_message")).add($("#pg_idea")).add($("#pg_email"));
+  fields.each(function(){
+    $(this).val('');
+  })
 }
